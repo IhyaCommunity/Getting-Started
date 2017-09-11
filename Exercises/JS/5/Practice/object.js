0d50dcbcdf3593d5a255eb2a192b2ba14ef4f24b
Hannan <hannanakbar@outlook.com>
2017-09-11 21:33:54 +0500

    Objects simplified

---
+++ Exercises/JS/5/Practice/object.js
@@ -0,0 +1,20 @@
+var classRoom1 = {
+    name: 'My Classroom',
+    students: 10,
+    duration: 4,
+    ongoing: true
+};
+
+var classRoom2 = classRoom;
+classRoom1.students = 5;
+
+console.log(classRoom1.students);
+console.log(classRoom2.students);
+
+// var name1 = 'Ali';
+// var name2 = name1;
+
+// name2 = 'Abuzar';
+
+// console.log(name1);
+// console.log(name2);
